7f170352f85bf3d799d157b52e8a52c9137e730a
Phil Quintanilla <pvquintanilla@gmail.com>
2021-01-18 18:28:42 -0700

    started coding for app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// reference table body
+var tbody=d3.select("tbody");
+
+// loop through data for values and table append
+tableData.forEach((ufoSighting)=> {
+    console.log(ufoSighting)
+
+    var row=tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value])=> {
+        console.log(key, value);
+
+        var cell=row.append("td");
+        cell.text(value);
+    });
+})
