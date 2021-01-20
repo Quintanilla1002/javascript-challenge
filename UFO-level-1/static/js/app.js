20adc759470c03a0193f11f2b8e92d776395e8b0
Phil Quintanilla <pvquintanilla@gmail.com>
2021-01-19 19:47:00 -0700

    finished ufo-level-1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,17 +2,44 @@
 var tableData = data;
 
 // reference table body
-var tbody=d3.select("tbody");
+var tbody = d3.select("tbody");
 
 // loop through data for values and table append
-tableData.forEach((ufoSighting)=> {
+tableData.forEach((ufoSighting) => {
     console.log(ufoSighting)
 
-    var row=tbody.append("tr");
-    Object.entries(ufoSighting).forEach(([key, value])=> {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
         console.log(key, value);
 
-        var cell=row.append("td");
+        var cell = row.append("td");
         cell.text(value);
     });
+});
+
+// select submit button
+var submit = d3.select("#filter-btn");
+
+//click event
+submit.on("click", function() {
+    d3.select("tbody").html("");
+    d3.event.preventDefault();
+
+    var dateTime=d3.select("#datetime").property("value");
+    console.log(dateTime);
+
+    var filteredData = tableData.filter(sighting => sighting.datetime === dateTime);
+    console.log(filteredData);
+
+    filteredData.forEach((ufoSighting) => {
+        var row = tbody.append('tr');
+        Object.entries(ufoSighting).forEach(([key, value]) => {
+            console.log(key, value);
+
+            var cell=row.append("td");
+            cell.text(value);
+        });
+    });
 })
+
+
